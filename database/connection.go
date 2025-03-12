d56911320e8c7fc71032f36edd3dcbda00be2ce8
Seyhmus <seyhmusoksak@gmail.com>
2025-03-13 00:44:32 +0300

    Fix: Database connection issue.

--- database/connection.go
+++ database/connection.go
@@ -14,19 +14,15 @@ var DB *gorm.DB
 func ConnectDB() (error) {
 	env := godotenv.Load()
 	if env != nil {
-		fmt.Println("Error loading .env file")
 		return env
 	}
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_HOST"),
 	os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
-	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		fmt.Println("Failed to connect to database:", err)
 		return err
 	}
-	err = DB.AutoMigrate(&models.Collection{}, &models.Todo{})
-	if err != nil {
-		fmt.Println("Failed to migrate database:", err)
-	}
+	db.AutoMigrate(&models.Collection{}, &models.Todo{})
+	DB = db
 	return nil
 }
